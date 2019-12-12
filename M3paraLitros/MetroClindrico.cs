bc0f5b24a8406250f53488ae5b5b1e1b7ad69a4e
gilmar <jiraspiom@hotmail.com>
2019-12-12 10:05:52 -0300

    Adicionado a classe responsavel de calculo. e adicionado inicio.

---
+++ M3paraLitros/MetroClindrico.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace M3paraLitros
+{
+    class MetroClindrico
+    {
+        private double raio;
+        private double altura;
+        private double resultado { get; set; }
+        private double resultadolitro { get; set; }
+
+        public MetroClindrico(double raio, double altura)
+        {
+            this.raio = raio;
+            this.altura = altura;
+            Metro();
+        }
+
+        private void Metro()
+        {
+            this.resultado = Math.PI * Math.Pow(this.raio, 2) * this.altura;
+            this.resultadolitro = Math.Round(resultado * 1000, 3);
+        }
+
+        public override string ToString()
+        {
+            return
+                "M3 " + resultado.ToString() + "\r\n" +
+                "Litro " + resultadolitro.ToString();
+        }
+    }
+}
--- M3paraLitros/Program.cs
+++ M3paraLitros/Program.cs
@@ -6,7 +6,22 @@ namespace M3paraLitros
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Os valores devem ser digitado com virgula em metros:");
+            Console.WriteLine("ex: 13 cm = 0,13;");
+            Console.WriteLine("    1 metro = 1,00;)");
+            Console.WriteLine("    1 metro e 42 cm = 1,42");
+
+
+            Console.Write("Digite o raio: ");
+            double raio = Double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura: ");
+            double altura = Double.Parse(Console.ReadLine());
+
+            MetroClindrico nome = new MetroClindrico(raio, altura);
+
+            Console.WriteLine(nome);
         }
+
     }
 }
